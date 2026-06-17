2fab5a472674e20631d95af1f2d4790de6b18078
RT-Prog <ramasamy.t@gmail.com>
2026-06-16 22:56:49 -0700

    config: Bind Flask to 0.0.0.0 to allow local network connections

--- bq-releases-notes/app.py
+++ bq-releases-notes/app.py
@@ -92,4 +92,5 @@ def get_releases():
     return jsonify({'success': True, 'data': entries})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
